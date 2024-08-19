7e2ba03f5536929dceb96287aec1009a3c272fca
chantithya <102397977+chantithya@users.noreply.github.com>
2024-08-19 11:01:52 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import InstagramFeed from './components/InstagramFeed';
-import InstagramNewFeed from './components/InstagramNewFeed';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import InstagramFeed from './InstagramFeed';
+import InstagramNewFeed from './InstagramNewFeed';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
+const App = () => (
+  <div className="App">
+    <Router basename="/liyao-instagram-updates">
+      <Routes>
         <Header />
-        <main className="container my-5">
-          <Routes>
+          <main className="container my-5">
             <Route path="/" element={<InstagramFeed />} />
-            <Route path="/post/:id" element={<InstagramNewFeed />} /> {/* Correct path */}
-          </Routes>
-        </main>
+            <Route path="/post/:id" element={<InstagramNewFeed />} />
         <Footer />
-      </Router>
-    </div>
-  );
-}
+      </Routes>
+    </Router>
+  </div>
+);
 
 export default App;
